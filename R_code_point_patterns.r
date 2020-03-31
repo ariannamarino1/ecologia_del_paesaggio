c8953ad449624448f2430af8396d5bd3ec2f6bcd
ariannamarino1 <62289261+ariannamarino1@users.noreply.github.com>
2020-03-31 13:03:17 +0200

    Create R_code_point_patterns.r

---
+++ R_code_point_patterns.r
@@ -0,0 +1,53 @@
+# Codice per analisi dei point patterns
+
+setwd("C:/lab")  
+setwd("C:/Users/asus/Desktop/Lezioni/Ecologia del paesaggio/lab")
+# importare dati
+covid<-read.table("covid_agg.csv",header=TRUE")
+# per vedere la tabella
+head(covid)
+
+plot(covid$country,covid$cases)
+#oppure
+attach(covid)
+plot(country,cases)
+
+#disposizione delle etichette
+plot(covid$country,covid$cases,las=0) #etichette parallele all'asse x e y (parallel labels)
+plot(covid$country,covid$cases,las=1) #etichette orizzontali (horizontal labels)
+plot(covid$country,covid$cases,las=2) #etichette perpendicolari all'asse x e y (perpendicular labels)
+plot(covid$country,covid$cases,las=3) #etichette verticali (vertical labels)
+
+plot(covid$country,covid$cases,las=3,cex.axis=0.3) #cex.axis comando per cambiare le dimensione delle etichette
+
+
+#ggplot2
+# richiamare la library ggplot2
+library(ggplot2)
+data(mpg)
+head(mpg)
+
+# data 
+# aes
+# tipo di geometria
+ggplot(mpg,aes(x=displ,y=hwy))+geom_point()
+ggplot(mpg,aes(x=displ,y=hwy))+geom_line()
+ggplot(mpg,aes(x=displ,y=hwy))+geom_polygon()
+
+
+#ggplot di covid
+ggplot(covid,aes(x=lon,y=lat,size=cases))+geom_point()
+
+
+#richiamiamo la librery spatstat
+library(spatstat)
+
+#density
+#create datased for spatstat
+attach(covid)
+covids<-ppp(lon,lat,c(-180,180),c(-90,90))   #>ppp(x.coordinates,y.coordinates,x.range,y.range)
+
+d<-density(covids)
+
+plot(d)
+points(covids) 
