4e3db2d76b2ddfcbfa48bc0359a9a03877d50b1d
ariannamarino1 <62289261+ariannamarino1@users.noreply.github.com>
2020-04-07 18:26:19 +0200

    Update R_code_primocod.r

--- R_code_primocod.r
+++ R_code_primocod.r
@@ -27,3 +27,21 @@ pairs(meuse[,3:6],col="blue",pch=18)
 pairs(meuse[,3:6],col="blue",pch=18,cex=3)
 
 pairs(meuse[,3:6],col="blue",pch=18,cex=3,main="Primo pairs")
+
+pairs(meuse[,3:6],lower.panel=panel.correlations,upper.panel=panel.smoothing,diag.panel=panel.histograms)
+
+# EXERCISE: mettere come lower panel lo smoothing, come diagonal panel gli istogrammi e come upper panel le correlazioni
+
+pairs(meuse[,3:6],lower.panel=panel.smoothing,upper.panel=panel.correlations,diag.panel=panel.histograms)
+
+## funzione plot
+
+plot(meuse$cadmium,meuse$copper)
+
+attach(meuse)
+
+plot(cadmium,copper)
+
+plot(cadmium,copper,pch=17,col="green",main="Primo plot")
+
+plot(cadmium,copper,pch=17,col="green",main="Primo plot",xlab="cadmio",ylab="copper")
